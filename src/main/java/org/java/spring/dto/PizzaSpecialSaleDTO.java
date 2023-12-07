5f2149d39ed2723fb0ffce98480e14cb49c4ab26
Deltafortal <aldolucchetta21@gmail.com>
2023-12-07 12:43:50 +0100

    Added dto

---
+++ src/main/java/org/java/spring/dto/PizzaSpecialSaleDTO.java
@@ -0,0 +1,25 @@
+package org.java.spring.dto;
+
+public class PizzaSpecialSaleDTO {
+
+	
+	private int pizza_id;
+	private String nome;
+	
+	
+	
+	public int getPizza_id() {
+		return pizza_id;
+	}
+	public void setPizza_id(int pizza_id) {
+		this.pizza_id = pizza_id;
+	}
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
